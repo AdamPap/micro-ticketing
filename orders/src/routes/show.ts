29aa17a9b85eba817e14d8e1917de2561b9564be
AdamPap <adampapamast@hotmail.com>
2021-07-04 01:40:29 +0300

    added tests and routes for orders

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,29 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@adamptickets/common";
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send({});
+  }
+);
 
 export { router as showOrderRouter };
