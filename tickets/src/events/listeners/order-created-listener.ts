bb58015dfcd602cfa1c6b61dee2b1c4dcbcb130d
AdamPap <adampapamast@hotmail.com>
2021-07-08 22:12:22 +0300

    added publish with event data

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -7,6 +7,7 @@ import {
 } from "@adamptickets/common";
 import { queueGroupName } from "./queue-group-name";
 import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -22,7 +23,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     ticket.set({ orderId: data.id });
     ticket.save();
 
-    new TicketUpdatedPublisher(this.client);
+    // await so that we dont ack if an error occurs
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
 
     msg.ack();
   }
