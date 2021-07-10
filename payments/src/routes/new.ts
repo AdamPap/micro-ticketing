1f81caad984909285050dac434a5d480ad50b61b
AdamPap <adampapamast@hotmail.com>
2021-07-11 01:54:05 +0300

    updated new payment route and added tests

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,15 @@
-import { requireAuth } from "@adamptickets/common";
+import {
+  BadRequestError,
+  currentUser,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@adamptickets/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -8,7 +17,24 @@ router.post(
   "/api/payments",
   requireAuth,
   [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
+  validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (req.currentUser!.id !== order.userId) {
+      throw new NotAuthorizedError();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError("Cannot pay for a cancelled order");
+    }
+
     res.send({ success: true });
   }
 );
