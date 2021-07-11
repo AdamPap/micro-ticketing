5f64f5af5c2b9316ac3624682ca5dcd5e53a3adb
AdamPap <adampapamast@hotmail.com>
2021-07-12 00:18:01 +0300

    added payment created event and publisher

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -4,7 +4,7 @@ import { OrderStatus } from "@adamptickets/common";
 import { app } from "../../app";
 import { Order } from "../../models/order";
 import { stripe } from "../../stripe";
-// import { Payment } from "../../models/payment";
+import { Payment } from "../../models/payment";
 
 it("returns a 404 when purchasing an order that does not exist", async () => {
   await request(app)
@@ -93,9 +93,9 @@ it("returns a 201 with valid inputs", async () => {
   expect(stripeCharge).toBeDefined();
   expect(stripeCharge!.currency).toEqual("eur");
 
-  // const payment = await Payment.findOne({
-  //   orderId: order.id,
-  //   stripeId: stripeCharge!.id,
-  // });
-  // expect(payment).not.toBeNull();
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+  expect(payment).not.toBeNull();
 });
