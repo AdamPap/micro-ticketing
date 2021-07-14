04223aefce15a2c33efc3a4f51c2c103f22283c6
AdamPap <adampapamast@hotmail.com>
2021-07-14 03:29:47 +0300

    added stripe checkout

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,18 @@
 import { useState, useEffect } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import useRequest from "../../hooks/use-request";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+
+  const { doRequest, errors } = useRequest({
+    url: "/api/payments",
+    method: "post",
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -25,7 +36,13 @@ const OrderShow = ({ order }) => {
     <div>
       <h2>Ticket: {order.ticket.title}</h2>
       <h4>Order expires in: {timeLeft} seconds</h4>
-      <h6>Order status: {order.status}</h6>
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey="pk_test_51JC1UPKxynnrJ1iq0pSp1HP4PXOxrMGbnMyyyB351JvzYXL4WOgzodNbcuQL0HvSXbi7COyfa2q6pq57z6ak5pMT00bImJJloI"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
     </div>
   );
 };
